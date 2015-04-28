e1c2ad5de57eb0316ff02e8b2570e22148e797b1
Jawn <jawnnypoo@gmail.com>
2015-04-28 00:04:49 -0500

    Update the sample activity to test new changes

--- app/src/main/java/com/jawnnypoo/physicslayout/sample/MainActivity.java
+++ app/src/main/java/com/jawnnypoo/physicslayout/sample/MainActivity.java
@@ -1,19 +1,22 @@
 package com.jawnnypoo.physicslayout.sample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 
 import com.jawnnypoo.physicslayout.PhysicsLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     PhysicsLayout physicsLayout;
     SwitchCompat physicsSwitch;
+    SwitchCompat flingSwitch;
+    View impulseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         physicsLayout = (PhysicsLayout) findViewById(R.id.physics_layout);
         physicsSwitch = (SwitchCompat) findViewById(R.id.physics_switch);
+        flingSwitch = (SwitchCompat) findViewById(R.id.fling_switch);
+        impulseButton = findViewById(R.id.impulse_button);
         physicsSwitch.setChecked(true);
         physicsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -32,6 +37,19 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+        flingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                physicsLayout.setFling(isChecked);
+            }
+        });
+        impulseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                physicsLayout.giveRandomImpulse();
+            }
+        });
+
     }
 
 
