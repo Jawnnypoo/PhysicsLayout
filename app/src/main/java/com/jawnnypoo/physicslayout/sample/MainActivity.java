fb0487b05174f276e116d59c5a34029a03e6320b
Jawn <jawnnypoo@gmail.com>
2015-05-05 22:25:49 -0500

    Sample project now uses fragments and shows a sort of real world scenario in addition to the test fragment.

--- app/src/main/java/com/jawnnypoo/physicslayout/sample/MainActivity.java
+++ app/src/main/java/com/jawnnypoo/physicslayout/sample/MainActivity.java
@@ -1,105 +1,55 @@
 package com.jawnnypoo.physicslayout.sample;
 
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-import com.jawnnypoo.physicslayout.PhysicsConfig;
-import com.jawnnypoo.physicslayout.PhysicsRelativeLayout;
-import com.squareup.picasso.Picasso;
+import com.astuetz.PagerSlidingTabStrip;
 
 
 public class MainActivity extends BaseActivity {
 
-    Toolbar toolbar;
-    PhysicsRelativeLayout physicsLayout;
-    SwitchCompat physicsSwitch;
-    SwitchCompat flingSwitch;
-    View impulseButton;
-    View addViewButton;
-
-    int catIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
-        setSupportActionBar(toolbar);
 
-        physicsLayout = (PhysicsRelativeLayout) findViewById(R.id.physics_layout);
-        physicsSwitch = (SwitchCompat) findViewById(R.id.physics_switch);
-        flingSwitch = (SwitchCompat) findViewById(R.id.fling_switch);
-        impulseButton = findViewById(R.id.impulse_button);
-        addViewButton = findViewById(R.id.add_view_button);
-        physicsSwitch.setChecked(physicsLayout.getPhysics().isPhysicsEnabled());
-        physicsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    physicsLayout.getPhysics().enablePhysics();
-                } else {
-                    physicsLayout.getPhysics().disablePhysics();
-                    for (int i=0; i<physicsLayout.getChildCount(); i++) {
-                        physicsLayout.getChildAt(i)
-                                .animate().translationY(0).translationX(0).rotation(0);
-                    }
-                }
-            }
-        });
-        flingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    physicsLayout.getPhysics().enableFling();
-                } else {
-                    physicsLayout.getPhysics().disableFling();
-                }
-            }
-        });
-        impulseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                physicsLayout.getPhysics().giveRandomImpulse();
-            }
-        });
-        final View circleView = findViewById(R.id.circle);
-        addViewButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new TabsAdapter(getSupportFragmentManager()));
+        PagerSlidingTabStrip pagerSlidingTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pagerSlidingTabStrip.setViewPager(viewPager);
+    }
 
-                ImageView imageView = new ImageView(MainActivity.this);
-                imageView.setImageResource(R.drawable.ic_launcher);
-                LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(
-                        getResources().getDimensionPixelSize(R.dimen.square_size),
-                        getResources().getDimensionPixelSize(R.dimen.square_size));
-                imageView.setLayoutParams(llp);
-                physicsLayout.addView(imageView);
-                Picasso.with(MainActivity.this)
-                        .load("http://lorempixel.com/200/200/cats/" + ((catIndex % 10) + 1))
-                        .placeholder(R.drawable.ic_launcher)
-                        .into(imageView);
-                catIndex++;
-            }
-        });
-        PhysicsConfig config = new PhysicsConfig.Builder()
-                .setShapeType(PhysicsConfig.ShapeType.CIRCLE)
-                .build();
-        physicsLayout.getPhysics().setPhysicsConfig(circleView, config);
+    public static class TabsAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
 
-
-        for (int i=0; i<physicsLayout.getChildCount(); i++) {
-            ImageView view = (ImageView) physicsLayout.getChildAt(i);
-            Picasso.with(this)
-                    .load("http://lorempixel.com/200/200/cats/" + (i + 1))
-                    .placeholder(R.drawable.ic_launcher)
-                    .into(view);
+        public TabsAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
         }
-        catIndex = physicsLayout.getChildCount();
 
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return TestingFragment.newInstance();
+                case 1:
+                    return TypicalFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position == 0 ? "Testing" : "Your App";
+        }
     }
 }
